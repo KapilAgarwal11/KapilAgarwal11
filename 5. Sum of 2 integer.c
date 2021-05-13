4179a034231bba5dae4c5361205719f73abbe615
KapilAgarwal11 <83746815+KapilAgarwal11@users.noreply.github.com>
2021-05-13 19:28:54 +0530

    Create 5. Sum of 2 integer.c

---
+++ 5. Sum of 2 integer.c
@@ -0,0 +1,11 @@
+//Add two integer number
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter two integer value:");
+    scanf("%d %d",&a,&b);
+    //Add a+b
+    c = a+b;
+    printf("a+b = %d",c);
+    return 0;
+}
