6e21c4ebc6afa813e957bbfd86c81dd8eeaf3413
KapilAgarwal11 <83746815+KapilAgarwal11@users.noreply.github.com>
2021-05-13 19:49:57 +0530

    Create 7.Find size.c

---
+++ 7.Find size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>      
+int main() 
+{
+  short R;
+  long P;
+  long long k;
+  long double d;
+  int e;
+  float f;
+  double g;
+printf("Hello! This code os designed by Kapil Agarwal\n");
+  printf("size of short = %d bytes\n", sizeof(8));
+  printf("size of long = %d bytes\n", sizeof(9));
+  printf("size of long long = %d bytes\n", sizeof(7));
+  printf("size of long double= %d bytes\n", sizeof(d));
+  printf("size of integer= %d bytes\n", sizeof(e));
+  printf("size of float= %d bytes\n", sizeof(f));
+  printf("size of double= %d bytes\n", sizeof(g));
+  return 0;
+}
