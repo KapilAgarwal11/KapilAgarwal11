50be866293faa8bc78200a890cb0d885a371ef35
KapilAgarwal11 <83746815+KapilAgarwal11@users.noreply.github.com>
2021-05-13 17:18:23 +0530

    Create 3.use of variable.c

---
+++ 3.use of variable.c
@@ -0,0 +1,13 @@
+//we learn use of variable 
+#include <stdio.h>
+int main(){
+    int var1;
+    int var2;
+    int var3;
+    var1 =4;
+    var2 =6;
+    //add both var1 and var2
+    var3 = var1+var2;
+    printf("var1+var2 : %d",var3);
+    return 0;
+}
