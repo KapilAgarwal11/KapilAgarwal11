53d452535964c86152a3068239d3c986efcfcfca
KapilAgarwal11 <83746815+KapilAgarwal11@users.noreply.github.com>
2021-05-13 17:09:08 +0530

    Create Use of comments.c

---
+++ Use of comments.c
@@ -0,0 +1,11 @@
+//we learn c programe
+#include <stdio.h>
+int main(){
+    int a = 2,b = 3,c;
+    //c=a+b
+    //we add a and b 
+    c = a+b;
+    printf("we get the value c =\n");
+    printf("a+b : %d",c);
+    return 0;
+}
