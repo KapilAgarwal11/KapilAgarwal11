371bfd4f52cdc76844769a5f7c7657e5c37afb05
KapilAgarwal11 <83746815+KapilAgarwal11@users.noreply.github.com>
2021-05-13 17:24:19 +0530

    Create 4. Use of variable in one statement.c

---
+++ 4. Use of variable in one statement.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() 
+{
+    int var1=10, var2=20;
+    printf("Value of first and second variables are as follows: 1st-> %d and 2nd-> %d\n", var1, var2);
+    return 0;
+}
