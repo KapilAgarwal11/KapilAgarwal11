3fa407601e5b1ebc8edf671ed5ded3afba91c324
KapilAgarwal11 <83746815+KapilAgarwal11@users.noreply.github.com>
2021-05-16 17:05:26 +0530

    Create 8. Square of input no.in output.c

---
+++ 8. Square of input no.in output.c
@@ -0,0 +1,9 @@
+//input from the user
+#include <stdio.h>
+int main(){
+   int a;
+    printf("Enter the value of a to get square value:");
+    scanf("%d",&a);
+    printf("The square of given number is:%d",a*a);
+    return 0;
+}
